9cd1277741c39c91629cd5b1748d84c5eb82aa08
TehBrian <tehbrian@protonmail.com>
2021-12-18 02:08:26 -0500

    remove unnecessary comment

--- resources/js/main.js
+++ resources/js/main.js
@@ -62,7 +62,6 @@ function calculate() {
 }
 
 // Stolen straight from stack overflow. https://stackoverflow.com/a/4673436/
-// First, checks if it isn't implemented yet.
 if (!String.prototype.format) {
     String.prototype.format = function () {
         var args = arguments;
